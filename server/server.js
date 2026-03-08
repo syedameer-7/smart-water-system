ce87092759b5d48d1db9da6803da591bb2f31daa
Syed Ameer Basha <ameerong7@gmail.com>
2026-03-08 19:30:43 +0530

    fix wildcard route

--- server/server.js
+++ server/server.js
@@ -254,7 +254,7 @@ const path = require("path");
 // serve frontend
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
