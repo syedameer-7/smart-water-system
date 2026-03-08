f6cf4b087c419de9c8afa14c7a1a818489c6cac1
Syed Ameer Basha <ameerong7@gmail.com>
2026-03-08 19:36:02 +0530

    fix fallback route

--- server/server.js
+++ server/server.js
@@ -254,7 +254,7 @@ const path = require("path");
 // serve frontend
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("/*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
